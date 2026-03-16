b170d31527bc0b6acde1ff61203e1ed3aac1f5d3
Michael Emmanuel <myke7104@gmail.com>
2026-03-16 15:28:38 +0100

    Refactor: move variable declarations from backend.tf to dedicated variables.tf in dev environment

---
+++ resources/dev/variables.tf
@@ -0,0 +1,52 @@
+# resources/dev/variables.tf
+
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name prefix for all resources"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "allowed_ssh_cidr" {
+  description = "Trusted IP CIDR allowed to SSH into the EC2 instance"
+  type        = string
+}
